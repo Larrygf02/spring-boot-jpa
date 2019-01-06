7fc7d705a1cf2a99316a8e58d2dcb1fd956aad9d
Larrygf02 <raulgf_02@hotmail.com>
2019-01-06 12:18:26 -0500

    Cambiando a CRUDRepository

--- src/main/java/com/example/springboot/app/models/dao/IClienteDao.java
+++ src/main/java/com/example/springboot/app/models/dao/IClienteDao.java
@@ -1,15 +1,8 @@
 package com.example.springboot.app.models.dao;
 
-import java.util.List;
-
+import org.springframework.data.repository.CrudRepository;
 import com.example.springboot.app.models.entity.Cliente;
 
-public interface IClienteDao {
-	public List<Cliente> findAll();
+public interface IClienteDao extends CrudRepository<Cliente, Long>{
 	
-	public void save(Cliente cliente);
-	
-	public Cliente findOne(Long id);
-	
-	public void delete(Long id);
 }
