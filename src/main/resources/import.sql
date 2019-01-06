5f24e4b9cbb2cdfc78a49adbc6ca62b6e409145e
Larrygf02 <raulgf_02@hotmail.com>
2019-01-06 11:22:18 -0500

    Project jpa

---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO clientes (id, nombre, apellido, email, created_at) VALUES (1,'Andres','Guzman', 'profesor@gmail.com','2018-10-12');
+INSERT INTO clientes (id, nombre, apellido, email, created_at) VALUES (2,'Raul','Larriega', 'raul@gmail.com','2018-10-13');
